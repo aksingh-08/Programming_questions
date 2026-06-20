72f25dd89b2c3facb5b115ffabf4df4f7ef9ab53
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-20 16:42:22 +0530

    update

--- even_odd.py
+++ even_odd.py
@@ -1,9 +1,14 @@
 # A number is even if divisible by 2 (remainder 0), otherwise it is edd.
 
+# def even_odd(n):
+#     if n % 2 == 0:
+#         print("given number is even.")
+#     else:
+#         print("given number is odd")
+# num = int(input("Enter the number: "))
+# even_odd(num)
+
 def even_odd(n):
-    if n % 2 == 0:
-        print("given number is even.")
-    else:
-        print("given number is odd")
+    return 'Even' if n % 2 == 0 else 'Odd'
 num = int(input("Enter the number: "))
-even_odd(num)
+print(even_odd(num))
