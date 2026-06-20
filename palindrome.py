d960124e8706a1ab9b7e870ef8c61ff6aa51281a
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-20 17:11:29 +0530

    update

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,20 @@
 # A number is a palindrome if it reads the same forwards and backwards. Example: 121, 1221,
 
+
+# num = int(input("Enter the number: "))
+# original_num = num
+# reversed_num = 0
+# while num > 0:
+#     digit = num % 10
+#     reversed_num = reversed_num * 10 + digit
+#     num //= 10
+# print(original_num == reversed_num)
+
+
 def is_palindrome(n):
     s = str(abs(n))
     print(s, s[::-1])
     return s == s[::-1]
 num = int(input("Enter the number: "))
 print(is_palindrome(num))
+
