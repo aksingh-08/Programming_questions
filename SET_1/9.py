8f3d84798e957f444fdb38a49ba261e3c3ce5225
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-28 10:56:25 +0530

    area of circle

---
+++ SET_1/9.py
@@ -0,0 +1,12 @@
+# Write a program to find area of a circle.
+
+# import math
+# radius = float(input('Enter the radius of the circle: '))
+# area = math.pi * radius ** 2
+# print(f'area of the circle = {area}')
+
+
+radius = float(input('enter the radius of the circle: '))
+pi = 3.14
+area = pi * radius ** 2
+print(f'area of the circle = {area}')
