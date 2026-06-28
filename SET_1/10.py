a5a3972d6619b1a54c74e1fbb4914e8cd26c8950
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-28 10:59:15 +0530

    area of rectangle

---
+++ SET_1/10.py
@@ -0,0 +1,6 @@
+# Write a program to find area of a rectangle.
+
+length = float(input('enter the length of the rectangle: '))
+width = float(input('enter the width of the rectangle: '))
+area = length * width
+print(f'area of the rectangle = {area}')
