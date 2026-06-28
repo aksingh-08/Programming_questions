d0c7db0938bf2b31d36b94530f1f1ad987ae35ff
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-28 17:17:52 +0530

    leap year or not

---
+++ SET_1/26.py
@@ -0,0 +1,20 @@
+# Write a program to identify id the year is leap year or not.
+
+
+# year = int(input("Enter a year: "))
+# if year % 400 == 0:
+#     print(year, "is a Leap Year.")
+# elif year % 100 == 0:
+#     print(year, "is not a Leap Year.")
+# elif year % 4 == 0:
+#     print(year, "is a Leap Year.")
+# else:
+#     print(year, "is not a Leap Year.")
+
+
+
+year = int(input("Enter a year: "))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(year, "is a Leap Year.")
+else:
+    print(year, "is not a Leap Year.")
