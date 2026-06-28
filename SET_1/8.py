4052c1b50d2af277da0003b5d7df98735538eef5
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-28 10:51:59 +0530

    even or odd

---
+++ SET_1/8.py
@@ -0,0 +1,7 @@
+# Write a program to identify if an program is even or odd.
+
+num = int(input("Enter an number: "))
+if num%2==0:
+    print('the number is even.')
+else:
+    print('the number is odd.')
