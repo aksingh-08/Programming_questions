a338efcc6a40e557b3a9a1527aedfe1e7ba9f668
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-28 10:38:38 +0530

    number state: positive or negative

---
+++ SET_1/7.py
@@ -0,0 +1,10 @@
+# Write a program to identify if a number is positive or negative.
+
+num = int(input("enter a number: "))
+if num>0:
+    print('the number is positive.')
+elif num<0:
+    print('the number is negative.')
+else:
+    print('the number s zero.')
+    
