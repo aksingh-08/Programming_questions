7a968b9a809d1496a5301169466f4f2cf374c884
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-01 00:03:49 +0530

    print solid and hollow rectangle star pattern

---
+++ SET_2/22.py
@@ -0,0 +1,23 @@
+# Write a program to print solid and hollow rectangle star pattern.
+
+
+# rows = int(input('enter the number of rows: '))
+# cols = int(input('enter the number of columns: '))
+# for i in range(rows):
+#     for j in range(cols):
+#         print("*", end=" ")
+#     print()
+
+
+
+
+rows = int(input('enter the number of rows: '))
+cols = int(input('enter the number of columns: '))
+for i in range(rows):
+    for j in range(cols):
+        if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+    
