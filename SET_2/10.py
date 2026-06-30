95fec21f25b1be7017dffa7b9f5aba855f74f76b
aksingh-08 <ashishk.singh1008@gmail.com>
2026-06-30 17:49:18 +0530

    convert digit to word

---
+++ SET_2/10.py
@@ -0,0 +1,19 @@
+# Write a program to convert digit/number to words.
+
+
+num = input('enter a number: ')
+words = {
+    '0': 'zero',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine'
+}
+for digit in num:
+    print(words[digit], end=" ")
+    
