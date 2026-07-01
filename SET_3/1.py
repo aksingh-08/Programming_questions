00e3e97d0f029b75f514bd2d2f8b3ced4bc2136b
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-01 10:33:52 +0530

    copy a string

---
+++ SET_3/1.py
@@ -0,0 +1,18 @@
+# Write a program to copy a string.
+
+
+original = input('enter a string: ')
+copy = original
+print(f'\noriginal string: {original}')
+print(f'copied string: {copy}')
+
+
+# original = input('enter a string: ')
+# copy = ""
+# for ch in original:
+#     copy += ch
+#     print(f'\noriginal string: {original}')
+#     print(f'copied string: {copy}')
+
+
+
