e1c24fd996e4201f6813cba363c8ca3c5ebee3b6
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-01 10:40:58 +0530

    concatenate a string

---
+++ SET_3/3.py
@@ -0,0 +1,9 @@
+# Write a program to concatenate a string.
+
+
+str1 = input('enter the first string: ')
+str2 = input('enter the second string: ')
+res = str1 + " " + str2
+# str1 += str2
+print(f'concatenated string: {res}')
+
