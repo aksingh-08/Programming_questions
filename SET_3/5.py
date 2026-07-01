514e48e0e75bcb3726c982ec6b95f6572779f3bc
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-01 10:46:11 +0530

    print length of a string

---
+++ SET_3/5.py
@@ -0,0 +1,14 @@
+# Write a program to print the length of a string.
+
+
+str = input('enter a string: ')
+length = len(str)
+print(f'length of the string: {length}')
+
+
+
+# str = input('enter a string: ')
+# count = 0
+# for ch in str:
+#     count += 1
+# print(f'length of the string: {count}')
