ac033b3a799cb8a86cfea50b99e6623e9854f077
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-02 10:46:41 +0530

    program to compare two strings

---
+++ SET_3/6.py
@@ -0,0 +1,11 @@
+# Write a program to compare two strings.
+
+
+str1 = input('enter the first string: ')
+str2 = input('enter the second string: ')
+if str1 == str2:
+    print('equal')
+else:
+    print('not equal')
+
+    
