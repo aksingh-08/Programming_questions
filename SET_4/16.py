416d14d6a9162c76b281726447eed11b0db38d90
aksingh-08 <ashishk.singh1008@gmail.com>
2026-07-08 00:05:06 +0530

    update: reversing array using two pointers

--- SET_4/16.py
+++ SET_4/16.py
@@ -1,8 +1,19 @@
 # Write a program for reversing an array.
 
 
-arr = list(map(int, input('enter array elements: ').split()))
-rev = []
-for _ in range(len(arr) - 1, -1, -1):
-    rev.append(arr[_])
-print(f'Reversed array: {rev}')
+# arr = list(map(int, input('enter array elements: ').split()))
+# rev = []
+# for _ in range(len(arr) - 1, -1, -1):
+#     rev.append(arr[_])
+# print(f'Reversed array: {rev}')
+
+
+
+arr = list(map(int, input("enter array elements: ").split()))
+left = 0
+right = len(arr) - 1
+while left < right:
+    arr[left], arr[right] = arr[right], arr[left]
+    left += 1
+    right -= 1
+print('reversed array: ', arr)
